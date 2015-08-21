3d47b1713269b3de7aff2e8e201ab32dca018591
cwerneck <claudialuciawerneck@gmail.com>
2015-08-21 19:48:40 -0300

    first test

--- cachematrix.R
+++ cachematrix.R
@@ -4,50 +4,50 @@
 # This function creates a special matrix object that can         #
 # cache its inverse                                              #
 #                                                                #
-#----------------------------------------------------------------#
+#--------------------------------------------------------C.W.----#
 
 makeCacheMatrix <- function(x = matrix()) {
 
+#initialize an empty matrix
+m<-matrix(,nrow=0, ncol=0)
 
+set<-function(y){
+  x<<-y
+  m<<-matrix(, nrow=0, ncol=0)
+}
+get<-function()x
+setinv<-function(solve) m<<-solve
+getinv<-function()m
 
-
-
-
-
-
-
-
-
-
-
-
-
+#create the list that is returned by the function
+list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
-#------------------------------------------------------------------#
-## This function tests if the inverse matrix exists                #
-## if exists, give a message and returns the Inverse Matrix cached #
-## if not, calculates the solv of the matrix                       #
-#------------------------------------------------------------------#
+#-----------------------------------------------------------------#
+# This function tests if the inverse matrix exists in cache       #
+# if yes, gives a message and returns the cached inverse matrix   #
+# if not, calculates the solv of the matrix                       #
+#--------------------------------------------------------C.W.-----#
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
 
+m<-x$getinv()
 
+# tests if the cached inverse matrix is empty
+if ((ncol(m)>0) || (nrow(m)>0)){
+# if NOT empty, returns the cached inverse matrix 
+  message("getting cached inverse matrix")
+  return (m)
+}
+# if the inverse matrix is empty, will calculate 
+data<-x$get()
 
-
-
-
-
-
-
-
-
-#--  inverse the matrix 
+#--  the inverse matrix 
 
    m<-solve(data)
-
-#--  m is the R object that contains the inverse matrix and is the product of the function
-
+   x$setinv(m)
+   
+#--  m is the R object that contains the inverse matrix 
+#--  and is the product of the function
    m
 }
