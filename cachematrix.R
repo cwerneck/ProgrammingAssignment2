e6af68678e4a2422b507da12cbe7bf9442b51f95
cwerneck <claudialuciawerneck@gmail.com>
2015-08-21 18:08:53 -0300

    first test

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+#----------------------------------------------------------------#
+#  R Programming - Assignment 2                                  #
+#                                                                #
+# This function creates a special matrix object that can         #
+# cache its inverse                                              #
+#                                                                #
+#----------------------------------------------------------------#
 
 makeCacheMatrix <- function(x = matrix()) {
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
-
-## Write a short comment describing this function
+#------------------------------------------------------------------#
+## This function tests if the inverse matrix exists                #
+## if exists, give a message and returns the Inverse Matrix cached #
+## if not, calculates the solv of the matrix                       #
+#------------------------------------------------------------------#
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+
+
+
+
+
+
+
+
+
+
+#--  inverse the matrix 
+
+   m<-solve(data)
+
+#--  m is the R object that contains the inverse matrix and is the product of the function
+
+   m
 }
